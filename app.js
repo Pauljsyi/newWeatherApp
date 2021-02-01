b2807d40244fe4f5eb0073143377b284690a2bce
Paul Yi <pauljsyi@gmail.com>
2021-02-01 14:45:09 -0800

    created .env file to store important info & parsed response data

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const request = require('request')
-
-const url = 'http://api.weatherstack.com/current?access_key=&query=Los%Angeles#'
-
-request({ url: url }, (error, response) => {
-    console.log(response.body.current)
+const express = require('express')
+require('dotenv').config('./../bin/dev');
 
 
+// var api = '415b7647c01d36e1c576ca58b514567e'
+var api =process.env.API_KEY
+
+const url = `http://api.weatherstack.com/current?access_key=${api}&query=los%angeles&units=f`
+
+request({ url: url, json:true }, (error, response) => {
+    // const body = JSON.parse(response.body)
+    const body = response.body
+    const current = body.current
+    const icon = body.current.weather_icons[0]
+    const description = body.current.weather_descriptions[0]
+    // console.log(description)
+    console.log(`${description}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out`)
     // const data = JSON.parse(response.body)
     // console.log(data.current)
 })
+
