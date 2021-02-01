fdc1385184e0cadd705af4096db04b2c0f81ed5e
Paul Yi <pauljsyi@gmail.com>
2021-01-31 20:59:56 -0800

    api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=f2906a2f99ef14b2f118384824a00335&query=Los%Angeles#'
+const url = 'http://api.weatherstack.com/current?access_key=&query=Los%Angeles#'
 
 request({ url: url }, (error, response) => {
     console.log(response.body.current)
