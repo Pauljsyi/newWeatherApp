6834c14c1ff761748cafff536116fb17111d7118
Paul Yi <pauljsyi@gmail.com>
2021-02-02 16:45:11 -0800

    callback exercise

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,10 +1,25 @@
 setTimeout(() => {
     console.log('Two seconds are up')
-}, 10000)
+}, 2000)
 
 const names = ['Ash', 'Jessie', 'James'];
 const shortNames = names.filter((name) => {
     return name.length <= 4
 })
 
-console.log(names)
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        }
+         callback(data)
+    }, 2000)
+   
+
+}
+
+geocode('Philadelphia', (data) => {
+    console.log(data)
+})
