184ecd213b2b0f7fbd83a9582cc9b5e004570558
Paul Yi <pauljsyi@gmail.com>
2021-02-02 21:12:10 -0800

    asynchronous callbacks for error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,28 +7,28 @@ var api =process.env.API_KEY
 
 const url = `http://api.weatherstack.com/current?access_key=${api}&query=Los%Angeles&units=f`
 
-request({ url: url, json:true }, (error, response) => { 
+// request({ url: url, json:true }, (error, response) => { 
 
-    if (error) {
-        console.log('WeatherStack: You are not connected to the internet')
-    } else if (response.body.success === false){
-        console.log("Can't find location...")
-    } else { 
-        const body = response.body
-        const current = body.current
-        const icon = body.current.weather_icons[0]
-        const description = body.current.weather_descriptions[0]
-        const city = body.location.name 
-        const region = body.location.region
+//     if (error) {
+//         console.log('WeatherStack: You are not connected to the internet')
+//     } else if (response.body.success === false){
+//         console.log("Can't find location...")
+//     } else { 
+//         const body = response.body
+//         const current = body.current
+//         const icon = body.current.weather_icons[0]
+//         const description = body.current.weather_descriptions[0]
+//         const city = body.location.name 
+//         const region = body.location.region
     
-            console.log(`${body.location.localtime} \n${description} in ${city}. \nIt is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out`)
+//             console.log(`${body.location.localtime} \n${description} in ${city}. \nIt is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out`)
 
-    } 
-})
+//     } 
+// })
 
 //success output
     const geoCode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY29kZXh4cHJlcyIsImEiOiJja2tvaTljaTYwYWEzMm5vNmRpankxa25hIn0.j0BCUl69x1Eu7g1TNHMnjQ&limit=1'
-    
+
 // cannot find coordinates error
     // const geoCode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/324567.json?access_token=pk.eyJ1IjoiY29kZXh4cHJlcyIsImEiOiJja2tvaTljaTYwYWEzMm5vNmRpankxa25hIn0.j0BCUl69x1Eu7g1TNHMnjQ&limit=1'
 
@@ -46,3 +46,22 @@ request( {url: geoCode, json:true}, (error, response) => {
         console.log('latitude:', latitude, 'longitude:', longitude)
     }
 } )
+
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiY29kZXh4cHJlcyIsImEiOiJja2tvaTljaTYwYWEzMm5vNmRpankxa25hIn0.j0BCUl69x1Eu7g1TNHMnjQ&limit=1'
+    console.log(url)
+
+    request({ url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Cannot connect', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('cannot find coordinates, try another search', undefined)
+        }
+    })
+}
+
+geocode('nononsdfsadfo', (error, data)=>{
+console.log('error', error)
+console.log('data', data)
+})
